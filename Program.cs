a40e7efddf794b46d348662d9e664c672cd5afb9
Modeling Complex <complexfieldmod@outlook.com>
2021-10-11 11:05:59 +0300

    The solution is stable

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ namespace wavemodel
         static void Main(/*string[] args*/)
         {
             PDESolver pdeSolver = new PDESolver();
-            pdeSolver.Init(0.001, 300, 300, 300, 300);
+            pdeSolver.Init(0.0003, 300, 300, 300, 300);
             pdeSolver.SetCoefficients(1000, 1500);
-            double graphStep = 0.1;
+            double graphStep = 0.02;
             double graphTime = graphStep;
 
             while (pdeSolver.GettCurrent() < 2)
