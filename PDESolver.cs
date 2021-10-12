d124500806293e08c32fac83ea34df54eb20b52a
PHtos <ph8htos@gmail.com>
2021-10-12 13:34:07 +0300

    sync

--- PDESolver.cs
+++ PDESolver.cs
@@ -10,7 +10,13 @@ namespace wavemodel
 
         double[,] Vx;
         double[,] Vy;
-        double[,] P;
+        double[,] P0;
+
+        /* https://github.com/nagataniyoshiki/FDTD_processing/blob/master/acoustic_2d_fdtd_processing2/acoustic_2d_fdtd_processing2.pde */
+        double[,] Mur_X1;   // Mur's 2nd-Order Absorption Layer
+        double[,] Mur_X2;   // Mur's 2nd-Order Absorption Layer
+        double[,] Mur_Y1;   // Mur's 2nd-Order Absorption Layer
+        double[,] Mur_Y2;   // Mur's 2nd-Order Absorption Layer
 
         double tCurrent;
         double dt;
@@ -18,10 +24,15 @@ namespace wavemodel
         double dy;
         double ro;
         double velocity;
-        // double lX;
-        // double lY;
+
         int Nx;
         int Ny;
+
+        double dt_dx_ro = 0;
+        double dt_dy_ro = 0;
+
+        double dt_dx_vro = 0;
+        double dt_dy_vro = 0;
 
         #endregion
 
@@ -30,30 +41,30 @@ namespace wavemodel
         //x = 0 border
         static double MuX(double y, double t)
         {
-            return 0.0;
+            return 0;
         }
         //x = x_max border
         public double NuX(double y, double t)
         {
-            return 0.0;
+            return 0;
         }
         //y = 0
         public double MuY(double x, double t)
         {
-            return 0.0;
+            return 0;
         }
         //y = y_max
         public double NuY(double x, double t)
         {
-            return 0.0;
+            return 0;
         }
         public double Fi(double x, double y)
         {
-            return 0.0;
+            return 0;
         }
         public double Psi(double x, double y)
         {
-            return 0.0;
+            return 0;
         }
 
         #endregion
@@ -62,41 +73,85 @@ namespace wavemodel
 
         public void Init(double dt, double lX, double lY, int Nx, int Ny)
         {
-            this.Nx = Nx;
-            this.Ny = Ny;
+            this.Nx = (int)((double)Nx / lX);
+            this.Ny = (int)((double)Ny / lY);
             this.dt = dt;
 
-            // this.lX = lX;
-            this.dx = lX / Nx;
-            // this.lY = lY;
-            this.dy = lY / Ny;
+            this.dx = lX;
+            this.dy = lY;
 
             InitGrid();
         }
         void InitGrid()
         {
+            #region init P0, Vx, Vy
+
+            P0 = new double[Nx + 1, Ny + 1];
             Vx = new double[Nx + 1, Ny + 1];
             Vy = new double[Nx + 1, Ny + 1];
-            P = new double[Nx + 1, Ny + 1];
 
             for (int i = 0; i <= Nx; i++)
-            {
-                for (int j = 0; j <= Ny; j++)
-                {
+                for (int j = 0; j < Ny; j++)
                     Vx[i, j] = 0;
+
+            for(int i = 0; i < Nx; i++)
+                for(int j = 0; j <= Ny; j++)
                     Vy[i, j] = 0;
-                    P[i, j] = 0;
-                }
+
+            for(int i = 0; i < Nx; i++)
+                for(int j = 0; j < Ny; j++)
+                    P0[i, j] = 0;
+
+
+            dt_dx_ro = (dt / dx) * ro;
+            dt_dy_ro = (dt / dy) * ro;
+
+            dt_dx_vro = dt_dx_ro * velocity * velocity;
+            dt_dy_vro = dt_dy_ro * velocity * velocity;
+
+            #endregion
+
+            #region init Mur
+
+            Mur_X1 = new double[4, Ny + 1];
+            Mur_X2 = new double[4, Ny + 1];
+            Mur_Y1 = new double[Nx + 1, 4];
+            Mur_Y2 = new double[Nx + 1, 4];
+
+            for(int i = 0; i <= Nx; i++)
+            {
+                Mur_Y1[i, 0] = 0;
+                Mur_Y1[i, 1] = 0;
+                Mur_Y1[i, 2] = 0;
+                Mur_Y1[i, 3] = 0;
+                Mur_Y2[i, 0] = 0;
+                Mur_Y2[i, 1] = 0;
+                Mur_Y2[i, 2] = 0;
+                Mur_Y2[i, 3] = 0;
             }
+
+            for(int j = 0; j <= Ny; j++)
+            {
+                Mur_X1[0, j] = 0;
+                Mur_X1[1, j] = 0;
+                Mur_X1[2, j] = 0;
+                Mur_X1[3, j] = 0;
+                Mur_X2[0, j] = 0;
+                Mur_X2[1, j] = 0;
+                Mur_X2[2, j] = 0;
+                Mur_X2[3, j] = 0;
+            }
+
+            #endregion
+
             tCurrent = 0;
         }
 
         double F(int i, int j, double t)
         {
             if( (Math.Abs( i - Nx / 2) <= 2) && (Math.Abs(j - Ny / 2) <= 2) && t < 0.2)
-            {
-                return Math.Cos(Math.PI * 2 * t);
-            }
+                return (double)Math.Cos(Math.PI * 2 * t);
+
             return 0;
         }
         public void SetCoefficients(double ro, double velocity)
@@ -109,129 +164,269 @@ namespace wavemodel
 
         public void CalcNextStep()
         {
-            
             UpdateV();
-            FillBoundaries();
             UpdateP();
-            FillBoundaries();
+
+            FillBoundaries_null();
+            //FillBoundaries_1ndOrder();
+            //FillBoundaries_2ndOrder();
 
             tCurrent += dt;
         }
-        private void FillBoundaries()
+
+        #region Boundaries
+
+        private void FillBoundaries_null()
         {
-            //for(int i = 0; i <= Nx; i++)
-            //{
-            //     double x = dx * i;
-
-            //    Vx[i, 0] = MuY(x, tCurrent);
-            //    Vy[i, 0] = MuY(x, tCurrent);
-            //    P[i, 0] = MuY(x, tCurrent);
-            //    P[i, 1] = MuY(x, tCurrent);
-            //    Vx[i, 1] = MuY(x, tCurrent);
-            //    Vy[i, 1] = MuY(x, tCurrent);
-
-            //    //Vx[i, Ny] = NuY(x, tCurrent);
-            //    //Vy[i, Ny] = NuY(x, tCurrent);
-            //    Vx[i, Ny] = 0;
-            //    Vy[i, Ny] = 0;
-            //    P[i, Ny] = P[i, Ny-1];
-            //}
-
-            //for (int j = 0; j <= Ny; j++)
-            //{
-            //     double y = dy * j;
-
-            //    Vx[0, j] = MuX(y, tCurrent);
-            //    Vx[Nx, j] = NuX(y, tCurrent);
-
-            //    Vy[0, j] = MuX(y, tCurrent);
-            //    Vy[Nx, j] = NuX(y, tCurrent);
-
-            //    P[0, j] = MuX(y, tCurrent);
-            //    P[Nx, j] = NuX(y, tCurrent);
-            //}
-            for (int j = 0; j <= Ny; j++)
+            for(int j = 0; j <= Nx; j++)
             {
-                P[0, j] = 0;
-                P[1, j] = 0;
+                P0[0, j] = 0;
+                P0[Nx, j] = 0;
                 Vx[0, j] = 0;
-                Vx[1, j] = 0;
+                Vx[Nx, j] = 0;
                 Vy[0, j] = 0;
-                Vy[1, j] = 0;
+                Vy[Nx, j] = 0;
             }
 
+            for(int i = 0; i <= Ny; i++)
+            {
+                P0[i, 0] = 0;
+                P0[i, Ny] = 0;
+                Vx[i, 0] = 0;
+                Vx[i, Ny] = 0;
+                Vy[i, 0] = 0;
+                Vy[i, Ny] = 0;
+            }
         }
 
+        private void FillBoundaries_2ndOrder()
+        {
+            int i = 0, j = 0;
+
+            #region Mur's 2nd Order Absorption
+
+            for(i = 2; i < Nx - 2; i++)
+            {
+                P0[i, 0] =
+                            -Mur_Y2[i, 1]
+                            + (velocity * dt - dx) / (velocity * dt + dx) * (P0[i, 1] + Mur_Y2[i, 0])
+                            + (2 * dx) / (velocity * dt + dx) * (Mur_Y1[i, 0] + Mur_Y1[i, 1])
+                            + (dx * velocity * velocity * dt * dt) / (2 * dx * dx * (velocity * dt + dx))
+                                * ( + Mur_Y1[i + 1, 0] - 2 * Mur_Y1[i, 0]
+                                    + Mur_Y1[i - 1, 0] + Mur_Y1[i + 1, 1]
+                                    - 2 * Mur_Y1[i, 1] + Mur_Y1[i - 1, 1]);
+
+                P0[i, Ny - 1] =
+                            -Mur_Y2[i, 2]
+                            + (velocity * dt - dx) / (velocity * dt + dx) * (P0[i, Ny - 2] + Mur_Y2[i, 3])
+                            + (2 * dx) / (velocity * dt + dx) * (Mur_Y1[i, 3] + Mur_Y1[i, 2])
+                            + (dx * velocity * velocity * dt * dt) / (2 * dx * dx * (velocity * dt + dx))
+                                * ( + Mur_Y1[i + 1, 3] - 2 * Mur_Y1[i, 3]
+                                    + Mur_Y1[i - 1, 3] + Mur_Y1[i + 1, 2]
+                                    - 2 * Mur_Y1[i, 2] + Mur_Y1[i - 1, 2]);
+            }
+
+            for(j = 2; j < Ny - 2; j++)
+            {
+                P0[0, j] =
+                            -Mur_X2[1, j]
+                            + (velocity * dt - dx) / (velocity * dt + dx) * (P0[1, j] + Mur_X2[0, j])
+                            + (2 * dx) / (velocity * dt + dx) * (Mur_X1[0, j] + Mur_X1[1, j])
+                            + (dx * velocity * velocity * dt * dt) / (2 * dx * dx * (velocity * dt + dx))
+                                * ( + Mur_X1[0, j + 1] - 2 * Mur_X1[0, j]
+                                    + Mur_X1[0, j - 1] + Mur_X1[1, j + 1]
+                                    - 2 * Mur_X1[1, j] + Mur_X1[1, j - 1]);
+
+                P0[Nx - 1, j] =
+                            -Mur_X2[2, j]
+                            + (velocity * dt - dx) / (velocity * dt + dx) * (P0[Nx - 2, j] + Mur_X2[3, j])
+                            + (2 * dx) / (velocity * dt + dx) * (Mur_X1[3, j] + Mur_X1[2, j])
+                            + (dx * velocity * velocity * dt * dt) / (2 * dx * dx * (velocity * dt + dx))
+                                * ( + Mur_X1[3, j + 1] - 2 * Mur_X1[3, j]
+                                    + Mur_X1[3, j - 1] + Mur_X1[2, j + 1]
+                                    - 2 * Mur_X1[2, j] + Mur_X1[2, j - 1]);
+            }
+
+            #endregion
+
+            #region Mur's 1st Order Absorption for 4 corners
+
+            i = 1;
+
+            P0[i, 0] = Mur_Y1[i, 1] + (velocity * dt - dx) / (velocity * dt + dx) * (P0[i, 1] - Mur_Y1[i, 0]);
+            P0[i, Ny - 1] = Mur_Y1[i, 2] + (velocity * dt - dx) / (velocity * dt + dx) * (P0[i, Ny - 2] - Mur_Y1[i, 3]);
+
+
+            i = Nx - 2;
+
+            P0[i, 0] = Mur_Y1[i, 1] + (velocity * dt - dx) / (velocity * dt + dx) * (P0[i, 1] - Mur_Y1[i, 0]);
+            P0[i, Ny - 1] = Mur_Y1[i, 2] + (velocity * dt - dx) / (velocity * dt + dx) * (P0[i, Ny - 2] - Mur_Y1[i, 3]);
+
+
+            j = 1;
+
+            P0[0, j] = Mur_X1[1, j] + (velocity * dt - dx) / (velocity * dt + dx) * (P0[1, j] - Mur_X1[0, j]);
+            P0[Nx - 1, j] = Mur_X1[2, j] + (velocity * dt - dx) / (velocity * dt + dx) * (P0[Nx - 2, j] - Mur_X1[3, j]);
+
+
+            j = Ny - 2;
+
+            P0[0, j] = Mur_X1[1, j] + (velocity * dt - dx) / (velocity * dt + dx) * (P0[1, j] - Mur_X1[0, j]);
+            P0[Nx - 1, j] = Mur_X1[2, j] + (velocity * dt - dx) / (velocity * dt + dx) * (P0[Nx - 2, j] - Mur_X1[3, j]);
+
+            #endregion
+
+            #region Copy Previous Values
+
+            for(i = 0; i < Nx; i++)
+            {
+                // Copy 1st Old Values to 2nd Old Values
+                Mur_Y2[i, 0] = Mur_Y1[i, 0];
+                Mur_Y2[i, 1] = Mur_Y1[i, 1];
+                Mur_Y2[i, 2] = Mur_Y1[i, 2];
+                Mur_Y2[i, 3] = Mur_Y1[i, 3];
+
+                // Copy Present Values
+                Mur_Y1[i, 0] = P0[i, 0];
+                Mur_Y1[i, 1] = P0[i, 1];
+                Mur_Y1[i, 2] = P0[i, Ny - 2];
+                Mur_Y1[i, 3] = P0[i, Ny - 1];
+            }
+
+            for(j = 0; j < Ny; j++)
+            {
+                // Copy 1st Old Values to 2nd Old Values
+                Mur_X2[0, j] = Mur_X1[0, j];
+                Mur_X2[1, j] = Mur_X1[1, j];
+                Mur_X2[2, j] = Mur_X1[2, j];
+                Mur_X2[3, j] = Mur_X1[3, j];
+
+                // Copy Present Values
+                Mur_X1[0, j] = P0[0, j];
+                Mur_X1[1, j] = P0[1, j];
+                Mur_X1[2, j] = P0[Nx - 2, j];
+                Mur_X1[3, j] = P0[Nx - 1, j];
+            }
+
+            #endregion
+
+
+            #region source
+
+            double freq = 1;
+
+            double Cos = Math.Cos(2 * Math.PI * freq * tCurrent);
+            double Sin = Math.Sin(2 * Math.PI * freq * tCurrent);
+            double Amp = (1 - Cos) / 2 * Sin;
+
+            if(tCurrent < ((double)1 / freq) / dt)
+                P0[Nx / 2, Ny / 2] += Amp;
+
+            #endregion
+        }
+
+        private void FillBoundaries_1ndOrder()
+        {
+            #region Mur's 1st Order Absorption
+
+            for(int i = 1; i < Nx - 1; i++)
+            {
+                P0[i, 0] = Mur_Y1[i, 1] + (velocity * dt - dx) / (velocity * dt + dx) * (P0[i, 1] - Mur_Y1[i, 0]);
+                P0[i, Ny - 1] = Mur_Y1[i, 2] + (velocity * dt - dx) / (velocity * dt + dx) * (P0[i, Ny - 2] - Mur_Y1[i, 3]);
+            }
+
+            for(int j = 1; j < Ny - 1; j++)
+            {
+                P0[0, j] = Mur_X1[1, j] + (velocity * dt - dx) / (velocity * dt + dx) * (P0[1, j] - Mur_X1[0, j]);
+                P0[Nx - 1, j] = Mur_X1[2, j] + (velocity * dt - dx) / (velocity * dt + dx) * (P0[Nx - 2, j] - Mur_X1[3, j]);
+            }
+
+            #endregion
+
+            #region Copy Previous Values
+
+            for(int i = 0; i < Nx; i++)
+            {
+                Mur_Y1[i, 0] = P0[i, 0];
+                Mur_Y1[i, 1] = P0[i, 1];
+                Mur_Y1[i, 2] = P0[i, Ny - 2];
+                Mur_Y1[i, 3] = P0[i, Ny - 1];
+            }
+            for(int j = 0; j < Ny; j++)
+            {
+                Mur_X1[0, j] = P0[0, j];
+                Mur_X1[1, j] = P0[1, j];
+                Mur_X1[2, j] = P0[Nx - 2, j];
+                Mur_X1[3, j] = P0[Nx - 1, j];
+            }
+
+            #endregion
+
+
+            #region source
+
+            double freq = 1;
+
+            double Cos = Math.Cos(2 * Math.PI * freq * tCurrent);
+            double Sin = Math.Sin(2 * Math.PI * freq * tCurrent);
+            double Amp = (1 - Cos) / 2 * Sin;
+
+            if(tCurrent < ((double)1 / freq) / dt)
+                P0[Nx / 2, Ny / 2] += Amp;
+
+            #endregion
+        }
+
+        #endregion
         //
 
         private void UpdateV()
         {
-            // dt_over_dx = (dt / dy) / ro;
-            // dt_over_dy = (dt / dy) / ro;
-
-            double dt_dx_ro = dt / (dx * ro);
-            double dt_dy_ro = dt / (dy * ro);
-
-            for (int i = 1; i < Nx; i++)
-            {
-                for (int j = 0; j < Ny; j++)
-                {
-                    Vx[i, j] -= dt_dx_ro * (P[i, j] - P[i - 1, j]);
-                }
-            }
-            for (int i = 0; i < Nx; i++)
-            {
-                for (int j = 1; j < Ny; j++)
-                {
-                    Vy[i, j] -= dt_dy_ro * (P[i, j] - P[i, j - 1]);
-                }
-            }
-        }
-
-        private void UpdateP()
-        {
-            //double dt_ro_c2x = dt * ro * velocity * 10 / dx; /*velocity*/
-            //double dt_ro_c2y = dt * ro * velocity * 10 / dy; /*velocity*/
-
-            double dt_dx_ro = (dt / dx) * ro * velocity * velocity;
-            double dt_dy_ro = (dt / dy) * ro * velocity * velocity;
+            for(int i = 1; i < Nx; i++)
+                for(int j = 0; j < Ny; j++)
+                    Vx[i, j] += -dt_dx_ro * (P0[i, j] - P0[i - 1, j]);
 
             for(int i = 0; i < Nx; i++)
-                for(int j = 0; j < Ny; j++)
-                {
-                    P[i, j] -=
-                        dt_dx_ro * (Vx[i + 1, j] - Vx[i, j]) +
-                        dt_dy_ro * (Vy[i, j + 1] - Vy[i, j]) -
-                        dt * this.F(i, j, tCurrent);
-                }
+                for(int j = 1; j < Ny; j++)
+                    Vy[i, j] += -dt_dy_ro * (P0[i, j] - P0[i, j - 1]);
         }
+        private void UpdateP()
+        {
+            for(int i = 0; i < Nx; i++)
+                for(int j = 0; j < Ny; j++)
+                    P0[i, j] +=
+                        -dt_dx_vro * (Vx[i + 1, j] - Vx[i, j]) +
+                        -dt_dy_vro * (Vy[i, j + 1] - Vy[i, j]) - dt * this.F(i, j, tCurrent);
+        }
+
 
         #region save
 
-        public void SaveCurrentValuesP(String fileName)
+        public void SaveCurrentValuesP0(String fileName)
         {
             using StreamWriter outWriter = new StreamWriter(fileName, false, System.Text.Encoding.Default);
 
-            for(int i = 0; i <= Nx; i += 1)
-                for(int j = 0; j <= Ny; j += 1)
-                    outWriter.WriteLine((dx * i + " " + dy * j + " " + P[i, j]).Replace(',', '.'));
+            for(int i = 0; i < Nx; i++)
+                for(int j = 0; j < Ny; j++)
+                    outWriter.WriteLine((dx * i + " " + dy * j + " " + P0[i, j]).Replace(',', '.'));
         }
         public void SaveCurrentValuesVx(String fileName)
         {
             using StreamWriter outWriter = new StreamWriter(fileName, false, System.Text.Encoding.Default);
 
-            for (int i = 0; i <= Nx; i += 1)
-                for (int j = 0; j <= Ny; j += 1)
+            for (int i = 0; i < Nx; i++)
+                for (int j = 0; j < Ny; j++)
                     outWriter.WriteLine((dx * i + " " + dy * j + " " + Vx[i, j]).Replace(',', '.'));
         }
         public void SaveCurrentValuesVy(String fileName)
         {
             using StreamWriter outWriter = new StreamWriter(fileName, false, System.Text.Encoding.Default);
 
-            for (int i = 0; i <= Nx; i += 1)
-                for (int j = 0; j <= Ny; j += 1)
+            for (int i = 0; i < Nx; i++)
+                for (int j = 0; j < Ny; j++)
                     outWriter.WriteLine((dx * i + " " + dy * j + " " + Vy[i, j]).Replace(',', '.'));
         }
+
         public double GettCurrent()
         {
             return tCurrent;
--- Program.cs
+++ Program.cs
@@ -7,8 +7,9 @@ namespace wavemodel
         static void Main(/*string[] args*/)
         {
             PDESolver pdeSolver = new PDESolver();
-            pdeSolver.Init(0.0003, 300, 300, 300, 300);
             pdeSolver.SetCoefficients(1000, 1500);
+            pdeSolver.Init(0.001, 10, 10, 500, 500);
+
             double graphStep = 0.01;
             double graphTime = graphStep;
 
@@ -19,7 +20,7 @@ namespace wavemodel
 
                 if (pdeSolver.GettCurrent() > graphTime)
                 {
-                    pdeSolver.SaveCurrentValuesP("outFixedP" +  Math.Round(graphTime, 2) + ".kr");
+                    pdeSolver.SaveCurrentValuesP0("outFixedP0" + Math.Round(graphTime, 2) + ".kr");
                     pdeSolver.SaveCurrentValuesVx("outFixedVx" + Math.Round(graphTime, 2) + ".kr");
                     pdeSolver.SaveCurrentValuesVy("outFixedVy" + Math.Round(graphTime, 2) + ".kr");
                     graphTime += graphStep;
