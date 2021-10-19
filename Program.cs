4b524c9300953dd5d7a14dee3d333442648b64f3
PHtos <ph8htos@gmail.com>
2021-10-19 17:17:19 +0300

    sync

--- Program.cs
+++ Program.cs
@@ -6,22 +6,24 @@ namespace wavemodel
     {
         static void Main(/*string[] args*/)
         {
-            int Nx = 300;
-            int Ny = 300;
-            int Nz = 300;
+            int Step = 10;
+            int Nx = 300 / Step;
+            int Ny = 300 / Step;
+            int Nz = 200 / Step;
 
             float [,,] velocity = new float[Nx, Ny, Nz];
-            for(int i = 0; i < Nz; i++)
-                for(int j = 0; j < Nz; j++)
+            for(int i = 0; i < Nx; i++)
+                for(int j = 0; j < Ny; j++)
                     for(int k = 0; k < Nz; k++)
                         velocity[i, j, k] = 1500 + k;
 
             PDESolver pdeSolver = new PDESolver();
-            pdeSolver.Init(0.0003f, 300, 300, 300, Nx, Ny, Nz);
+            pdeSolver.Init(0.0001f, Nx, Ny, Nz);
             pdeSolver.SetCoefficients(1000, velocity);
 
-            double graphStep = 0.01;
-            double graphTime = graphStep;
+            float graphStep = 0.01f;
+            float graphTime = graphStep;
+
 
             while(pdeSolver.GettCurrent() < 0.5)
             {
@@ -32,22 +34,11 @@ namespace wavemodel
                 if(pdeSolver.GettCurrent() > graphTime)
                 {
                     pdeSolver.SaveCurrentValuesP("outFixedP0" + Math.Round(graphTime, 2) + ".kr");
-                    // pdeSolver.SaveCurrentValuesVx("outFixedVx" + Math.Round(graphTime, 2) + ".kr");
-                    // pdeSolver.SaveCurrentValuesVy("outFixedVy" + Math.Round(graphTime, 2) + ".kr");
                     graphTime += graphStep;
                     pdeSolver.FlushCSV();
                 }
             }
-
             pdeSolver.Close();
-
-            /*
-            while (pdeSolver.GettCurrent() < 100)
-            {
-                pdeSolver.CalcNextStep();
-            }
-            pdeSolver.SaveCurrentValues("out.kr");
-            */
         }
     }
 }
